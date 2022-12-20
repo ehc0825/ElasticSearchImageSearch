b5344164d54dd6c4ebb5b152caa397df7471d16e
ehc0825 <ehc0825@naver.com>
2022-12-20 10:55:21 +0900

    최초 커밋

---
+++ src/test/java/com/ehc/ElasticSearchImageSearch/ElasticSearchImageSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehc.ElasticSearchImageSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchImageSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
