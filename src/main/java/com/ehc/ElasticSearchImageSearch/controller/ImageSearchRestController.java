cdf826326ef1be55c8fc94749c464326d156c21e
ehc0825 <ehc0825@naver.com>
2022-12-20 11:01:39 +0900

    기초 구조 생성

---
+++ src/main/java/com/ehc/ElasticSearchImageSearch/controller/ImageSearchRestController.java
@@ -0,0 +1,22 @@
+package com.ehc.ElasticSearchImageSearch.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ImageSearchRestController {
+
+
+    @PutMapping("/putImageFolder")
+    public void putImageFolder(){
+
+    }
+
+    @PostMapping("/searchImage")
+    public void searchImage(){
+
+    }
+
+
+}
---
+++ src/main/java/com/ehc/ElasticSearchImageSearch/service/ImageSearchService.java
@@ -0,0 +1,14 @@
+package com.ehc.ElasticSearchImageSearch.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageSearchService {
+
+    public void putImageService(){
+
+    }
+    public void searchImageService(){
+
+    }
+}
