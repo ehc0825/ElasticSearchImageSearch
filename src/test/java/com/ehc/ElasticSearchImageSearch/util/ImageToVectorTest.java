47019c98a30c10c9f803d9a5e064bf7c29353ce9
ehc0825 <ehc0825@naver.com>
2022-12-24 05:33:30 +0900

    라이브러리화 테스트

--- src/test/java/com/ehc/ElasticSearchImageSearch/util/ImageToVectorTest.java
+++ src/test/java/com/ehc/ElasticSearchImageSearch/util/ImageToVectorTest.java
@@ -1,8 +1,14 @@
 package com.ehc.ElasticSearchImageSearch.util;
 
+import com.ehc.ElasticSearchImageSearch.util.similarity.similarities.Cosine;
+import com.ehc.elastiknnSimilarityQuery.QueryBuilder;
+import com.ehc.elastiknnSimilarityQuery.SimilarityOption;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -26,6 +32,25 @@ class ImageToVectorTest {
         {
             System.out.println("image vector화 실패");
         }
+    }
+
+    @Test
+    void 라이브러리_테스트()
+    {
+        String imageUrl="https://dimg.donga.com/ugc/CDB/SHINDONGA/Article/62/65/e6/3e/6265e63e0bf7d2738276.jpg";
+        String vector=imageToVector.imageUrlToVector(imageUrl);
+        String vectorForQuery="";
+        Pattern pattern = Pattern.compile("(\\[)(.*?)(\\])");
+        Matcher matcher= pattern.matcher(vector);
+        while (matcher.find()) {
+            vectorForQuery=matcher.group(2);
+            if(matcher.group(1) ==  null)
+                break;
+        }
+        String[] vectors=vectorForQuery.split(",");
+        String query= QueryBuilder.buildKnnQuery(SimilarityOption.COSINE,"vector",0,10,vectors);
+        System.out.println(query);
+
     }
 
 
