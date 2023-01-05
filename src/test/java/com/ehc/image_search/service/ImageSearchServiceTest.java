739f871265aca4fb97f49aa3ec8e40bcf3f031b8
ehc0825 <ehc0825@naver.com>
2023-01-05 18:10:41 +0900

    테스트 케이스 추가

---
+++ src/test/java/com/ehc/image_search/service/ImageSearchServiceTest.java
@@ -0,0 +1,10 @@
+package com.ehc.image_search.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ImageSearchServiceTest {
+    //TODO:230105 서비스 테스트 케이스 작성
+
+}
